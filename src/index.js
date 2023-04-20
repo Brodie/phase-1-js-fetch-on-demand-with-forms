50849b291a98bbfd4ab5215fe08bbddac6f00948
Brodie Ashcraft <brodiea19@gmail.com>
2023-04-20 10:13:07 -0700

    fetch on demand modulurized

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-const init = () => {
-  
-}
+const init = (e) => {
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", handleSubmit);
+};
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
+
+// modulurized handle submit
+function handleSubmit(e) {
+  e.preventDefault();
+  const input = document.querySelector("#searchByID");
+
+  fetch(`http://localhost:3000/movies/${input.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const title = document.querySelector("#movieDetails h4");
+      const summary = document.querySelector("#movieDetails p");
+
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+    });
+}
